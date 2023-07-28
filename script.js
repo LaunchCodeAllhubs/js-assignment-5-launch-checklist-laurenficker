f52626a39cc8188f6cb29f10594d6ead194f108c
Lauren Ficker <laurenficker@mrc-a034.attlocal.net>
2023-07-28 14:24:25 -0500

    finished assignment

--- script.js
+++ script.js
@@ -2,15 +2,37 @@
 
 window.addEventListener("load", function() {
 
+    let form = document.querySelector("form");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        let pilotName = document.querySelector("input[name=pilotName]").value;
+        let copilotName = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoMass = document.querySelector("input[name=cargoMass]").value;
+
+        let faultyItems = document.getElementById("faultyItems");
+       
+        formSubmission(document, faultyItems, pilotName, copilotName, fuelLevel, cargoMass);
+        console.log(faultyItems)
+    });
+
+
+
+
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+       let destination = pickPlanet(listedPlanets);
+       addDestinationInfo(document, destination["name"], destination["diameter"], destination["star"], destination["distance"], destination["moons"], destination["image"]) 
+    })
    
+    
 });
